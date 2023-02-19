a2c802f239a022369e5a81f7a494a5aa4391c89e
sarahchow <chow.sarah@hotmail.com>
2023-02-19 18:03:36 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sarah Chow on 2023-02-19.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
