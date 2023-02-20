f72daaef04d1871c6740a9b9ff8bb0d4b72fc93a
sarahchow <chow.sarah@hotmail.com>
2023-02-19 22:12:08 -0500

    added toggle button for favourites. support multiple parks and data entries

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,53 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Sarah Chow on 2023-02-19.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter{ landmark in
+            // does not show favorites. logic: true if showFavoritesOnly variable is false
+            // shows favorites. logic: true if showFavoritesOnly variable is true AND landmark is favored
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+
+    var body: some View {
+        
+        NavigationView{
+            List{
+                // Only toggles, double check start value of showFavoritesOnly is the proper value before compiling
+                Toggle(isOn: $showFavoritesOnly){
+                    Text("Favorites Only")
+                }
+                // 'landmark in' is the iteration variable. 'landmarks' is the array from data
+                ForEach(filteredLandmarks){ landmark in
+            
+                    NavigationLink{
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self){ deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
--- Landmarks/Views/MapView.swift
+++ Landmarks/Views/MapView.swift
@@ -10,6 +10,8 @@ import MapKit
 
 struct MapView: View {
     
+    var coordinate: CLLocationCoordinate2D
+    
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.155_868),
         span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
@@ -18,11 +20,21 @@ struct MapView: View {
     
     var body: some View {
         Map(coordinateRegion: $region)
+            .onAppear{
+                setRegion(coordinate)
+            }
+    }
+    
+    private func setRegion(_ coordinate: CLLocationCoordinate2D){
+        region = MKCoordinateRegion(
+        center: coordinate,
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
     }
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.155_868))
     }
 }
